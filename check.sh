74ed2c979d3d6cb34b0068e89b1f3bd891d57c5c
LinuxSec <11514023+linuxsec@users.noreply.github.com>
2020-04-16 16:58:41 +0700

    update..

--- check.sh
+++ check.sh
@@ -3,9 +3,9 @@ if [ -z "$1" ]; then
     echo -e "Usage: $0 list.txt\n";
     exit
 fi
-    echo -e "Nakano Nino best waifu!\n";
-#cat $1 | xargs -P 100 curl -s -o /dev/null -I -w "[%{http_code}] - $1"
+
+echo -e "Nakano Nino best waifu!\n";  
 for hostlists in $(cat $1);
 do
-   echo $hostlists | xargs -P 100 curl -s -o /dev/null -I -w "[%{http_code}] - $hostlists\n"
+    echo $hostlists | xargs -P 100 curl -s -o /dev/null -I -w "[%{http_code}] - $hostlists\n"
 done
