e030a34f5222f7e3274aaa919d4f9d1776605e93
LinuxSec <11514023+linuxsec@users.noreply.github.com>
2022-10-20 21:50:52 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -5,12 +5,27 @@ echo -e "
 Bulk status code scanner
 Codename : EVA02 - Asuka Soryu\n"
 
+# Colors 
+red="\033[0;31m"    # Error / Issues
+green="\033[0;32m"  # Successful
+reset="\033[0m"     # Normal
+
 # Variable
 process=10
-statuscheck(){
-    curl --write-out "%{http_code} - $hostlists\n" --silent --output /dev/null $hostlists
+useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+statuscode(){
+    curl -H "User-Agent: $useragent" --connect-timeout 3 --write-out "%{http_code}" --silent --output /dev/null $hostlists
 }
-
+statusresult(){
+    if [[ $(statuscode) == '200' ]]; then
+	 	echo -e "${green}[$(statuscode)]${reset} - $hostlists"
+    elif
+        [[ $(statuscode) == '403' ]]; then
+	 	echo -e "${red}[$(statuscode)]${reset} - $hostlists"
+  	else
+ 		echo "[$(statuscode)] - $hostlists"
+    fi
+}
 if [ -z "$1" ]; then
     echo -e "Usage: $0 list.txt\n";
     exit 1
@@ -19,7 +34,7 @@ fi
 # Do the jobs
 for hostlists in $(cat $1);
 do
-    statuscheck &
+    statusresult &
     background=( $(jobs -p) )
     if (( ${#background[@]} == process )); then
         wait -n
