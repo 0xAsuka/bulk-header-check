a8e3b53e35bbbbe3375413b3c7496118c5b93b6c
LinuxSec <11514023+linuxsec@users.noreply.github.com>
2022-10-20 11:07:10 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo -e "Usage: $0 list.txt\n";
-    exit
-fi
 
 #Banner
 echo -e "
-Bulk status code scanner                  
-Codename : EVA02 - Asuka Soryu\n"  
+Bulk status code scanner
+Codename : EVA02 - Asuka Soryu\n"
 
+# Variable
 process=10
 statuscheck(){
     curl --write-out "%{http_code} - $hostlists\n" --silent --output /dev/null $hostlists
 }
+
+if [ -z "$1" ]; then
+    echo -e "Usage: $0 list.txt\n";
+    exit 1
+fi
+
+# Do the jobs
 for hostlists in $(cat $1);
 do
     statuscheck &
@@ -22,3 +26,7 @@ do
     fi
 done
 wait
+
+# Exit after done
+echo  -e "\nAll jobs done. Thank you"
+exit 0
